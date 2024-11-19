30e8a7ece807979b17e0b877f0fae4b110968a29
hardikc369 <hardikc369@gmail.com>
2024-11-19 22:50:16 +0530

    implemented code to fix test case

--- src/main/java/com/apptware/interview/stream/impl/DataReaderImpl.java
+++ src/main/java/com/apptware/interview/stream/impl/DataReaderImpl.java
@@ -3,6 +3,9 @@ package com.apptware.interview.stream.impl;
 import com.apptware.interview.stream.DataReader;
 import com.apptware.interview.stream.PaginationService;
 import jakarta.annotation.Nonnull;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,11 +34,19 @@ class DataReaderImpl implements DataReader {
   private @Nonnull Stream<String> fetchPaginatedDataAsStream() {
     log.info("Fetching paginated data as stream.");
 
+    int page = 1;
+    int pageSize = 500;
+    List<String> allRecords = new ArrayList<>();
+      List<String> response;
+      do {
+          response = paginationService.getPaginatedData(page, pageSize);
+          ++page;
+        allRecords.addAll(response);
+      } while(!response.isEmpty());
     // Placeholder for paginated data fetching logic
     // The candidate will add the actual implementation here
 
-    Stream<String> dataStream =
-        Stream.empty(); // Temporary, will be replaced by the actual data stream
+    Stream<String> dataStream = allRecords.stream();
     return dataStream.peek(item -> log.info("Fetched Item: {}", item));
   }
 }
